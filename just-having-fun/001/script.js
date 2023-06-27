0e399445300e2b8fa784581d66ce1bb41d2a1f92
#FF0050 <135780151+Joa0Gustav0@users.noreply.github.com>
2023-06-27 00:01:08 -0300

    applying what I've learned today... 26/06/2023

--- just-having-fun/001/script.js
+++ just-having-fun/001/script.js
@@ -1,1 +1,5 @@
+function CreateTask(){
+    var NewMessageContent = document.getElementById("send-messages-inputid").value
 
+    document.querySelector('div.message>p').innerText=String(NewMessageContent)
+}
