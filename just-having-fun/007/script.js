9d6e4f9b6715418e68686d8f8b2cb36adcbaa6df
#FF0050 <135780151+Joa0Gustav0@users.noreply.github.com>
2023-09-19 20:36:22 -0300

    📚Studying array functions | reduce()

--- just-having-fun/007/script.js
+++ just-having-fun/007/script.js
@@ -66,11 +66,51 @@ const convertTo = (requiredScale) => {
     })
     console.log(mapTemps)
 }
-convertTo("F")*/
+convertTo("F")
+
 //filter()
 //Removes elements that are not into a condition(filtering)
+//Ex.1:
 const numberArray = [57, 12, 16, 29, 18, 33, 15]
 
 var result = numberArray.filter((elem, i, array) => elem >= 16 && elem <= 32)
 
-console.log(result)
+console.log(result)*/
+
+//reduce()
+//reduces array's values into a unic one. Concatenate values.
+//Ex.1:
+const passengersPerStop = [8, 16, 5, 3, 19]
+
+var totalPassengers = passengersPerStop.reduce((prevQuantity, currentValue) => prevQuantity + currentValue)
+console.log(totalPassengers)
+
+//Ex.2:
+const costumers =
+[
+    {name: "Rose", age: 27, shopNum: 24},
+    {name: "Sara", age: 16, shopNum: 5},
+    {name: "Karol", age: 64, shopNum: 16},
+    {name: "Jorge", age: 37, shopNum: 12},
+    {name: "Gangplank", age: 70, shopNum: 8},
+    {name: "Edgar", age: 26, shopNum: 4},
+    {name: "Jenice", age: 17, shopNum: 9},
+    {name: "Abigail", age: 42, shopNum: 24},
+    {name: "Darcylaine", age: 48, shopNum: 20}
+]
+
+var over18Costumers = costumers.filter((costumer, i, array) => costumer.age >= 18)
+var secondAgeCostumers = over18Costumers.filter((costumer, i, array) => costumer.age < 30 && costumer.age >= 18)
+var middleAgeCostumers = over18Costumers.filter((costumer, i, array) => costumer.age >= 30 && costumer.age < 60)
+var thirdAgeCostumers = over18Costumers.filter((costumer, i, array) => costumer.age >= 60)
+var totalShopNum = costumers.reduce((prevValue, currentValue) => prevValue + currentValue.shopNum, 0)
+
+console.log("This costumers are over 18:")
+console.log(over18Costumers)
+console.log("This costumers are at second age:")
+console.log(secondAgeCostumers)
+console.log("This costumers are at middle age:")
+console.log(middleAgeCostumers)
+console.log("This costumers are at third age:")
+console.log(thirdAgeCostumers)
+console.log("The total number of buying by the current costumers is: " + totalShopNum)
